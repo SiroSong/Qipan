69b5128c6d3627002310b51f9f476c3e0ca88a53
雷欧奥特曼 <songjinyuan@7k7k.com>
2020-04-17 09:28:25 +0800

    up format code

--- src/components/qipan.js
+++ src/components/qipan.js
@@ -1,70 +1,70 @@
 import React, { Component } from 'react';
 
-export class Qipan extends Component{
-    
-    constructor(props){
-        super(props);
-        this.canvas = React.createRef();
-    }
+export class Qipan extends Component {
 
-    componentDidMount(){
-        let ctx = document.getElementById('can').getContext('2d');
+	constructor(props) {
+		super(props);
+		this.canvas = React.createRef();
+	}
 
-        // 棋盘底色
-        ctx.save();
-        ctx.fillStyle = '#faa755';
-        // ctx.fillStyle = '#5f5f5f';
-        ctx.fillRect(0,0,570,570);
-        ctx.restore();
-        //棋盘线
-        for(let i = 0; i<19;i++){
-            ctx.beginPath();
-            ctx.moveTo(15,30*i+15);
-            ctx.lineTo(555,30*i+15);
-            ctx.stroke();
-            ctx.closePath();
-            ctx.beginPath();
-            ctx.moveTo(30*i+15,15);
-            ctx.lineTo(30*i+15,555);
-            ctx.stroke();
-            ctx.closePath();
-        }
-        //棋盘点
-        ctx.beginPath();
-        ctx.arc(105,105,4,0,2*Math.PI);
-        ctx.fill();
-        ctx.beginPath();
-        ctx.arc(285,285,4,0,2*Math.PI);
-        ctx.fill();
-        ctx.beginPath();
-        ctx.arc(465,465,4,0,2*Math.PI);
-        ctx.fill();
-        ctx.beginPath();
-        ctx.arc(105,285,4,0,2*Math.PI);
-        ctx.fill();
-        ctx.beginPath();
-        ctx.arc(105,465,4,0,2*Math.PI);
-        ctx.fill();
-        ctx.beginPath();
-        ctx.arc(285,105,4,0,2*Math.PI);
-        ctx.fill();
-        ctx.beginPath();
-        ctx.arc(285,465,4,0,2*Math.PI);
-        ctx.fill();
-        ctx.beginPath();
-        ctx.arc(465,105,4,0,2*Math.PI);
-        ctx.fill();
-        ctx.beginPath();
-        ctx.arc(465,285,4,0,2*Math.PI);
-        ctx.fill();
-    }
+	componentDidMount() {
+		let ctx = document.getElementById('can').getContext('2d');
 
-    render(){
-        return(
-                <canvas width={570} height={570} id={'can'} className={'qipan'}>
-                    您的浏览器不支持该棋盘，请更换高版本浏览器。
-                </canvas>
-            
-        )
-    }
+		// 棋盘底色
+		ctx.save();
+		ctx.fillStyle = '#faa755';
+		// ctx.fillStyle = '#5f5f5f';
+		ctx.fillRect(0, 0, 570, 570);
+		ctx.restore();
+		//棋盘线
+		for (let i = 0; i < 19; i++) {
+			ctx.beginPath();
+			ctx.moveTo(15, 30 * i + 15);
+			ctx.lineTo(555, 30 * i + 15);
+			ctx.stroke();
+			ctx.closePath();
+			ctx.beginPath();
+			ctx.moveTo(30 * i + 15, 15);
+			ctx.lineTo(30 * i + 15, 555);
+			ctx.stroke();
+			ctx.closePath();
+		}
+		//棋盘点
+		ctx.beginPath();
+		ctx.arc(105, 105, 4, 0, 2 * Math.PI);
+		ctx.fill();
+		ctx.beginPath();
+		ctx.arc(285, 285, 4, 0, 2 * Math.PI);
+		ctx.fill();
+		ctx.beginPath();
+		ctx.arc(465, 465, 4, 0, 2 * Math.PI);
+		ctx.fill();
+		ctx.beginPath();
+		ctx.arc(105, 285, 4, 0, 2 * Math.PI);
+		ctx.fill();
+		ctx.beginPath();
+		ctx.arc(105, 465, 4, 0, 2 * Math.PI);
+		ctx.fill();
+		ctx.beginPath();
+		ctx.arc(285, 105, 4, 0, 2 * Math.PI);
+		ctx.fill();
+		ctx.beginPath();
+		ctx.arc(285, 465, 4, 0, 2 * Math.PI);
+		ctx.fill();
+		ctx.beginPath();
+		ctx.arc(465, 105, 4, 0, 2 * Math.PI);
+		ctx.fill();
+		ctx.beginPath();
+		ctx.arc(465, 285, 4, 0, 2 * Math.PI);
+		ctx.fill();
+	}
+
+	render() {
+		return (
+			<canvas width={570} height={570} id={'can'} className={'qipan'}>
+				您的浏览器不支持该棋盘，请更换高版本浏览器。
+			</canvas>
+
+		)
+	}
 }
--- src/components/qizi.js
+++ src/components/qizi.js
@@ -1,84 +1,84 @@
 import React from 'react';
 
-export class Qizi extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            color: true,
-            qizis:[]
-        }
-        this.ctx = {};
-    }
+export class Qizi extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			color: true,
+			qizis: []
+		}
+		this.ctx = {};
+	}
 
-    componentDidMount(){
-        this.ctx= document.getElementById('qizi').getContext('2d');
-    }
+	componentDidMount() {
+		this.ctx = document.getElementById('qizi').getContext('2d');
+	}
 
-//获取棋子位置
-    clickHandle = (e) => {
-        let obj = e.target , x , y , row,col,nowqizis,canDown = true;
-        nowqizis = this.state.qizis;
-        x = e.clientX - obj.offsetLeft;
-        y = e.clientY - obj.offsetTop;
-        row = Math.round((x - 15)/30);
-        col = Math.round((y - 15)/30);
-        this.state.qizis.forEach( val => {
-            if(val.row === row && val.col === col){
-                canDown = false;
-            }
-        })
-        if(canDown){
-            nowqizis.push({
-                row: row,
-                col: col,
-                color:this.state.color,
-            });
-            this.setState({
-                color: !this.state.color,
-                qizis: nowqizis
-            },() => this.drawQizi());
-            canDown = true;
-        }
-    }
+	//获取棋子位置
+	clickHandle = (e) => {
+		let obj = e.target, x, y, row, col, nowqizis, canDown = true;
+		nowqizis = this.state.qizis;
+		x = e.clientX - obj.offsetLeft;
+		y = e.clientY - obj.offsetTop;
+		row = Math.round((x - 15) / 30);
+		col = Math.round((y - 15) / 30);
+		this.state.qizis.forEach(val => {
+			if (val.row === row && val.col === col) {
+				canDown = false;
+			}
+		})
+		if (canDown) {
+			nowqizis.push({
+				row: row,
+				col: col,
+				color: this.state.color,
+			});
+			this.setState({
+				color: !this.state.color,
+				qizis: nowqizis
+			}, () => this.drawQizi());
+			canDown = true;
+		}
+	}
 
-//描绘棋子
-    drawQizi = () => {
-        let that = this;
-        this.ctx.clearRect(0,0,570,570);
-        this.state.qizis.forEach((val) => {
-            that.ctx.beginPath();
-            that.ctx.arc(val.row*30+15,val.col*30+15,10,0,2*Math.PI);
-            that.ctx.fillStyle = val.color? 'white': 'black';
-            that.ctx.fill();
-            that.ctx.closePath();
-        })
-    }
+	//描绘棋子
+	drawQizi = () => {
+		let that = this;
+		this.ctx.clearRect(0, 0, 570, 570);
+		this.state.qizis.forEach((val) => {
+			that.ctx.beginPath();
+			that.ctx.arc(val.row * 30 + 15, val.col * 30 + 15, 10, 0, 2 * Math.PI);
+			that.ctx.fillStyle = val.color ? 'white' : 'black';
+			that.ctx.fill();
+			that.ctx.closePath();
+		})
+	}
 
-//悔棋
-    back = () => {
-        let nowqizis = this.state.qizis;
-        if(nowqizis.length){
-            nowqizis.pop();
-            this.setState({
-                qizis: nowqizis,
-                color: !this.state.color
-            },() => {
-                this.drawQizi();
-            })
-        }
-    }
+	//悔棋
+	back = () => {
+		let nowqizis = this.state.qizis;
+		if (nowqizis.length) {
+			nowqizis.pop();
+			this.setState({
+				qizis: nowqizis,
+				color: !this.state.color
+			}, () => {
+				this.drawQizi();
+			})
+		}
+	}
 
-    render(){
-        return(
-            <div className={'qipanContent'}>
-                <canvas id={'qizi'} width={570} height={570} onClick={this.clickHandle} className={'qizi'}>
-                    您的浏览器不支持该棋盘，请更换高版本浏览器。
+	render() {
+		return (
+			<div className={'qipanContent'}>
+				<canvas id={'qizi'} width={570} height={570} onClick={this.clickHandle} className={'qizi'}>
+					您的浏览器不支持该棋盘，请更换高版本浏览器。
                 </canvas>
-                <div className={'huiqi'}>
-                    <span>{this.state.color? '白棋回合': '黑棋回合'}</span>
-                    <button onClick={this.back}>悔棋</button>
-                </div>
-            </div>
-        )
-    }
+				<div className={'huiqi'}>
+					<span>{this.state.color ? '白棋回合' : '黑棋回合'}</span>
+					<button onClick={this.back}>悔棋</button>
+				</div>
+			</div>
+		)
+	}
 } 
